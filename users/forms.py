d881a84aec56701ce6aa51f142af73507bde931b
SergeyFoma <asdfg2105c@gmail.com>
2025-06-10 12:01:50 +0500

    style authoriz register

--- users/forms.py
+++ users/forms.py
@@ -26,8 +26,8 @@ class RegisterUserForm(UserCreationForm):
         }
 
 class LoginUserForm(AuthenticationForm):
-    username=forms.CharField(label="login", widget=forms.TextInput(attrs={"class":"form-input"}))
-    password=forms.CharField(label='password', widget=forms.PasswordInput(attrs={"class":"form-input"}))
+    username=forms.CharField(label="Логин", widget=forms.TextInput(attrs={"class":"form-input"}))
+    password=forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={"class":"form-input"}))
     class Meta:
         model=get_user_model()
         fields=['username', 'password']
