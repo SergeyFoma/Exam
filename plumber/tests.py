e852eff8e8bacb58a443b0ce58c0654e02a00b26
SergeyFoma <asdfg2105c@gmail.com>
2025-07-14 13:18:57 +0500

    tests1

--- plumber/tests.py
+++ plumber/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from http import HTTPStatus
+
+class GetPagesTestCase(TestCase):
+    def setUp(self):
+        "Инициализация перед выполнением каждого теста"
+    
+    def test_index(self):
+        path=reverse('plumber:index')
+        response=self.client.get(path)
+        #print(response)
+        #self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        #self.assertIn('plumber/index.html', response.template_name)
+        self.assertTemplateUsed(response, 'plumber/index.html')
+        self.assertEqual(response.context_data['title'],'Test online. Тестирование работников основных и смежных профессий')
+
+    # def test_redirect_logout(self):
+    #     path=reverse("users:logout_user")
+
+
+    def tearDown(self):
+        "Действие после выполнения каждого теста"
 
 # Create your tests here.
