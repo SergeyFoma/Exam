28b71790e901bb17056fb999d17d7d251c28e941
SergeyFoma <asdfg2105c@gmail.com>
2025-07-16 14:39:53 +0500

    cache end

--- exam/settings.py
+++ exam/settings.py
@@ -113,6 +113,7 @@ DATABASES = {
 # }
 CACHES = {
     "default": {
+        #"BACKEND": "django.core.cache.backends.dummy.DummyCache",
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379",
     }
